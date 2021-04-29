5ee8ee834691c31825013219dc94f6072834457d
wangchao <wangchao@bizseer.com>
2021-04-29 11:44:42 +0800

    引入antd

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Button } from 'antd';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
